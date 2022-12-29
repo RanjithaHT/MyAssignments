41622bf0bfaef32c9402acf6891f9b6ead19bbac
RanjithaHT <35480274+RanjithaHT@users.noreply.github.com>
2022-12-29 21:16:55 +0530

    Day2

---
+++ Day2/Fibbinocci.java
@@ -0,0 +1,20 @@
+package week1.Day2;
+
+public class Fibbinocci {
+
+	public static void main(String[] args) {
+		int firstNum = 0;
+		int	secNum = 1;
+		int sum = 0;
+		int i;
+		
+		
+		for (i=1;i<=11; ++i) {
+			
+		System.out.println(firstNum);
+		sum=firstNum+secNum;
+		firstNum=secNum;
+		secNum=sum;
+		}
+}
+}
